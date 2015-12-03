f7abb45b39024d37cdae967577b4d745c465a78d
Mirco Babini <mirkolofio@gmail.com>
2015-12-03 23:44:44 +0100

    day 1 *

---
+++ 1/1.js
@@ -0,0 +1,20 @@
+var fs = require('fs');
+fs.readFile('input.txt', 'utf-8', function (err, string) {
+    if (err) {
+        throw err;
+    }
+
+    var char_index = 0
+      , floor = 0
+      , basement_index = 0;
+
+    // search the final floor
+    for( char_index in string ){
+        if( string[ char_index ] === '(' ){
+            floor++;
+        }else{
+            floor--;
+        }
+    }
+    console.log( floor );
+});
