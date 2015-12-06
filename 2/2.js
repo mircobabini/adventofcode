12fca47b8b7bca9bcdb60a0a23a8314b50547917
mircobabini <mirkolofio@gmail.com>
2015-12-06 22:29:55 +0100

    day 2 **

--- 2/2.js
+++ 2/2.js
@@ -1,11 +1,13 @@
 var fs = require('fs');
 fs.readFile('input.txt', 'utf-8', function (err, string) {
-    var rows = string.split("\r\n");
+    // normalize carriage-return for cross-platform compatibility
+    string.replace(/[\n\r]/g, '\n');
+    var rows = string.split("\n");
 
-    var row;
+    var row, row_index;
     var abc = [];
     var feets_of_paper = 0;
-    for (var row_index in rows) {
+    for (row_index in rows) {
         row = rows[row_index];
         abc = row.split('x').map(returnInt);
         
@@ -20,8 +22,26 @@ fs.readFile('input.txt', 'utf-8', function (err, string) {
     }
     
     console.log( feets_of_paper );
+    
+    feets_of_paper = 0;
+    for (row_index in rows){
+        row = rows[row_index]; // 29x13x26
+        abc = row.split('x').map(returnInt); // abc = [29, 13, 26]
+        
+        var a = abc[0]  // 29
+          , b = abc[1]  // 13
+          , c = abc[2]; // 26
+          
+        var wrap = Math.min( (a+b)*2, (b+c)*2, (a+c)*2 );
+        var bow = a*b*c;
+        
+        feets_of_paper += wrap + bow;
+    }
+
+    console.log( feets_of_paper );
 });
 
 function returnInt(element) {
     return parseInt(element, 10);
 }
+
