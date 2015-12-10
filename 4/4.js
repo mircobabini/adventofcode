570be285333739cc062910c5f67c68c7a45c43c8
mircobabini <mirkolofio@gmail.com>
2015-12-11 00:07:58 +0100

    day 4 **

--- 4/4.js
+++ 4/4.js
@@ -3,18 +3,27 @@ var crypto = require('crypto');
 
 fs.readFile('input.txt', 'utf-8', function (err, secret) {
     var lowest_positive = 0;
-    
+    var lowest_positive_00000, lowest_positive_000000;
+
     while(true){
         lowest_positive++;
         
         var data = secret + lowest_positive;
         var hash = crypto.createHash('md5').update(data).digest("hex");
         
-        if( hash.substr( 0, 5 ) === '00000' ){
+        if( ! lowest_positive_00000 ){
+            if( hash.substr( 0, 5 ) === '00000' ){
+                lowest_positive_00000 = lowest_positive;
+            }
+        }
+
+        if( hash.substr( 0, 6 ) === '000000' ){
+            lowest_positive_000000 = lowest_positive;
             break;
         }
     }
     
-    console.log( lowest_positive );
+    console.log( lowest_positive_00000 );
+    console.log( lowest_positive_000000 );
 });
 
